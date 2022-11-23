695add77cb72dd3acf31bee7d4ee919750d4ad02
bestyoriya <gagaoxo5678@gmail.com>
2022-11-23 23:40:26 +0700

    b

---
+++ Lab 00/Lab 0.2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main () {
+	printf ( " Wake up at 6:00 \n " ) ;
+	printf ( " Take a shower, wash your face, brush your teeth \n " ) ;
+	printf ( " get dressed \n " ) ;
+	printf ( " have breakfast \n " ) ;
+	printf ( " drive refueling \n " ) ;
+	printf ( " go to university \n " ) ;
+	printf ( " to university \n " ) ;
+	printf ( " enter the classroom \n " ) ;
+	
+}//end function
