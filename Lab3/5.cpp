59c8217f50f9a971c3f59d405b3da47e500f0ecc
bestyoriya <gagaoxo5678@gmail.com>
2022-12-16 01:26:53 +0700

    Update 5.cpp

--- Lab3/5.cpp
+++ Lab3/5.cpp
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 struct example_struct {
+  int a;
+  float b;
+  char c;
 };
-int a; 
-float b;
-char c;
 
-void function(struct example_struct* example) {   
-	printf("a = %d\n", example->a); 
-	printf("b = %f\n", example->b); 
-	printf("c-  %c\n", example->c);
+void function(struct example_struct example) {
+  printf("a = %d\n", example.a);
+  printf("b = %f\n", example.b);
+  printf("c-  %c\n", example.c);
 }
 
-int main(){
-	struct example_struct example = {1, 2.5, 'c'};
-	
-	function(&example);
+int main() {
+  struct example_struct example = {a: 1, b: 4, c: 'A'};
 
-return 0;
+  function(example);
+
+  return 0;
 }//end function
